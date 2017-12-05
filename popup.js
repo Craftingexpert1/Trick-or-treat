0e9ade7e9bc96a0cab1653fb25a2a7da2e90ac9d
iamcev <mailofrealcev@gmail.com>
2017-12-05 18:38:02 -0500

    kdksj

--- popup.js
+++ popup.js
@@ -33,7 +33,7 @@ snackbar.show(dataObj);
            if (candyCount / 1000 < 1){
 			hmc.innerHTML = "How much candy you have:" + candyCount;	
 		}
-		if (candyCount / 1000 >= 1){
+		if (candyCount / 1000 > 1){
 			var x = candyCount / 1000;
 			x = Math.floor(x * 100) / 100;
 			x = x.toFixed(2);
